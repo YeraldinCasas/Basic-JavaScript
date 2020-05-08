8375fee80b95392552a7a342ca0b5641c60e5a49
YeraldinCasas <yeralgc@hotmail.com>
2020-05-08 03:18:39 -0500

    strings

--- basic.js
+++ basic.js
@@ -71,4 +71,22 @@ colores1.push("naranja");
 console.log(colores1)
 console.log(objeto1);
 
-
+//Cadenas de texto 
+let nombre = "Yeraldin";
+let apellido = 'Casas'; //No importa si se usan comillas dobles o sencillas siempre y cuando sea igul en todo el doc
+let saludo = new String("Hola mundo") //DEclaración formal con constructor 
+console.log(nombre, apellido,saludo)
+console.log("*************propiedad length******************");
+console.log(
+    nombre.length, 
+    apellido.length,
+    saludo.length,
+    nombre.toUpperCase(),
+    apellido.toLowerCase(),);
+ let lorem = "                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+ console.log(lorem.includes("dolor"), lorem.includes("Yeraldin"), lorem)
+ console.log(lorem.trim());
+ console.log(lorem.split(" "));
+ console.log(lorem.split(","));
+ console.log(lorem.charCodeAt([i]))
+    
